e20f0204dd3d0e9669b85cd1e67b1747122a03bf
许下星晴 <1625325214@qq.com>
2022-09-03 14:59:54 +0800

    完成了高级组件的配置--接下来功力大涨

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 module.exports = {
   // 1.配置方式一: CLI提供的属性
   outputDir: './build',
@@ -23,16 +21,4 @@ module.exports = {
       }
     }
   }
-  // configureWebpack: (config) => {
-  //   config.resolve.alias = {
-  //     '@': path.resolve(__dirname, 'src'),
-  //     components: '@/components'
-  //   }
-  // }
-  // 3.配置方式三:
-  // chainWebpack: (config) => {
-  //   config.resolve.alias
-  //     .set('@', path.resolve(__dirname, 'src'))
-  //     .set('components', '@/components')
-  // }
 }
